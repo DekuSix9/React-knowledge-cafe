734ae04cf1516946fc5071ea5357d49dbd2b2897
shamim <shamim@gmail.com>
2025-02-04 22:42:15 +0600

    setup and api data display

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+import Profile from '../..//assets/images/profile.png';
+
+
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center p-4  border-b-2 border-[#D9D9D9] max-w-7xl mx-auto'>
+            <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+            <img src={Profile}></img>
+        </header>
+    );
+};
+
+export default Header;
